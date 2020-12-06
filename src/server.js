034dfb43dd3e09f0f147b84160075a1553f008cd
magnus-hansson <hanssmag+git@gmail.com>
2020-12-06 13:40:56 +0100

    added server route

--- src/server.js
+++ src/server.js
@@ -1,17 +1,28 @@
-import sirv from 'sirv';
-import polka from 'polka';
-import compression from 'compression';
-import * as sapper from '@sapper/server';
+import sirv from "sirv";
+import polka from "polka";
+import compression from "compression";
+import * as sapper from "@sapper/server";
 
 const { PORT, NODE_ENV } = process.env;
-const dev = NODE_ENV === 'development';
+const dev = NODE_ENV === "development";
+
+function one(req, res, next) {
+  req.hello = "world";
+  next();
+}
 
 polka() // You can also use Express
+  .get("/users/:id", (req, res) => {
+    console.log(req.params.id);
+    res.end(`User: ${req.params.id}`);
+  })
   .use(
     compression({ threshold: 0 }),
-    sirv('static', { dev }),
-    sapper.middleware()
+    sirv("static", { dev }),
+    sapper.middleware(),
+    one
   )
+
   .listen(PORT, err => {
-    if (err) console.log('error', err);
+    if (err) console.log("error", err);
   });
